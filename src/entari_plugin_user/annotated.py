31fa292eceb80de8094033dac198cbd4ee734053
KomoriDev <mute231010@gmail.com>
2026-03-21 13:29:12 +0800

    :bookmark: v0.1.5

--- src/entari_plugin_user/annotated.py
+++ src/entari_plugin_user/annotated.py
@@ -2,9 +2,10 @@ from typing import Annotated
 
 from arclet.letoderea import Depends
 
+from .models import E
 from .models import User as _User
 from .models import UserSession as _UserSession
 from .depends import get_user, get_user_session
 
 User = Annotated[_User, Depends(get_user)]
-UserSession = Annotated[_UserSession, Depends(get_user_session)]
+UserSession = Annotated[_UserSession[E], Depends(get_user_session)]
